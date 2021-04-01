ac7b6c18e9528f4d65d6ede670f5eec0534d46fe
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2021-04-01 13:04:31 -0400

    Fixed some rendering issues on sign-up, created a basic login page.

--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -13,15 +13,15 @@ function NavBar() {
                         <i class="fas fa-cocktail"></i>
                     </Link>
                 </p>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> */}
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item" activeClassName="active-class-style">
+                    <li class="nav-item active">
                         <a class="nav-link" href="/sign-up">Sign Up</a>
                     </li>
-                    <li class="nav-item" activeClassName="active-class-style">
+                    <li class="nav-item" >
                         <a class="nav-link" href="/login">Login</a>
                     </li>
                 </ul>
--- src/component/SignUp/SignUp.js
+++ src/component/SignUp/SignUp.js
@@ -20,7 +20,7 @@ export class SignUp extends Component {
 
     // }
 
-    handleSignup = (event) => {
+    handleOnChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
         });
@@ -29,36 +29,38 @@ export class SignUp extends Component {
     
 
     render() {
+        const { firstName, lastName, email, password, confirmPassword, } = this.state;
+
         return (
             
-                <div className="form-body">
+                <body className="form-body">
                 <main class="form-signin">
             <form>
-                    <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="firstName" placeholder="First Name"/>
-                    <label for="floatingInput">First Name</label>
+                    <h1 class="h3 mb-3 fw-normal">Please Sign Up</h1>
+                <div>
+                    <input type="text" class="form-control" id="firstName" placeholder="First Name" name= "firstName" value={firstName} onChange={this.handleOnChange} pattern="[A-Za-z]*" required autoFocus/>
+                    <label htmlFor="inputFirstName" className="sr-only">First Name</label>
                 </div>
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="lastName" placeholder="Last Name"/>
-                    <label for="floatingInput">Last Name</label>
+                <div>
+                    <input type="text" class="form-control" id="lastName" placeholder="Last Name" name="lastName" value={lastName} onChange={this.handleOnChange} pattern="[A-Za-z]*" required autoFocus />
+                    <label htmlFor="inputLastName" className="sr-only">Last Name</label>
                 </div>
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="email" placeholder="Email Address"/>
-                    <label for="floatingInput">Email Address</label>
+                <div>
+                    <input type="text" class="form-control" id="email" placeholder="Email Address"  name="email" value={email} onChange={this.handleOnChange} required autoFocus />
+                    <label htmlFor="inputEmail" className="sr-only">Email Address</label>
                 </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="password" placeholder="Password"/>
-                    <label for="floatingPassword">Password</label>
+                <div>
+                    <input type="password" class="form-control" id="password" placeholder="Password" name="password" value={password} onChange={this.handleOnChange} required />
+                    <label htmlFor="inputPassword" className="sr-only">Password</label>
                 </div>
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="confirmPassword" placeholder="Confirm Password"/>
-                    <label for="floatingPassword">Confirm Password</label>
+                <div>
+                    <input type="text" class="form-control" id="confirmPassword" placeholder="Confirm Password" name="confirmPassword" value={confirmPassword} onChange={this.handleOnChange} required />
+                    <label htmlFor="inputConfirmPassword" className="sr-only">Confirm Password</label>
                 </div>
                 <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
             </form>
                 </main>
-                </div>
+                </body>
         )
     }
 }
